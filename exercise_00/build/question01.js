b92772729a5294cb40c88e173268cf3f33df9c89
Igor Gabriel <catce.20181ads0418@aluno.ifpi.edu.br>
2022-11-27 23:43:57 -0300

    Atualizando repositório.

--- exercise_00/build/question01.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let num = 10.0;
-console.log(`Antecessor ${num - 1} e sucessor ${num + 1}.`);
--- exercise_00/build/question02.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let real = 10.0;
-let bitcoin = real * 0.000011;
-console.log(`O valor de ${real} em Bitcoin é de ${bitcoin.toFixed(11)}.`);
